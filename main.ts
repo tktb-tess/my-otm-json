62e22f30ef7007396b18fe6667adc41825590e5d
tktb-tess <chemusiclove6174@gmail.com>
2026-01-01 17:00:48 +0900

    haha

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
+import Otmjson from "./vl_ja.json" with { type: "json" };
 
+const words = Otmjson.words;
+
+for (const word of words) {
+    const pro = word.contents.find((c) => c.title === 'Pronunciation');
+    if (!pro) {
+        continue;
+    }
+    const prev = pro.text;
+    pro.text = prev.replaceAll('ɤ', 'ə').replaceAll('aɪ', 'ɐɪ').replaceAll('aʊ', 'ɑʊ');
+}
+
+Deno.writeTextFileSync('./output.json', JSON.stringify(Otmjson, null, 2));
