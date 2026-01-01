a7ae617bf630f477efe5769bd59349ba29d4e81b
tktb-tess <chemusiclove6174@gmail.com>
2026-01-01 18:55:20 +0900

    sorted by id

--- main.ts
+++ main.ts
@@ -1,31 +1,19 @@
-import Otmjson from "./vl_ja.json" with { type: "json" };
+import otmjson from "./vl_ja.json" with { type: "json" };
 
-const words = Otmjson.words;
+const words = otmjson.words;
 
 for (const word of words) {
-    const pro = word.contents.find((c) => c.title === 'Pronunciation');
-    if (!pro) {
-        continue;
-    }
-    const prev = pro.text;
-    pro.text = prev.replaceAll('ɤ', 'ə').replaceAll('aɪ', 'ɐɪ').replaceAll('aʊ', 'ɑʊ');
+  const pro = word.contents.find((c) => c.title === "Pronunciation");
+  if (!pro) {
+    continue;
+  }
+  const prev = pro.text;
+  pro.text = prev.replaceAll("ɤ", "ə").replaceAll("aɪ", "ɐɪ").replaceAll(
+    "aʊ",
+    "ɑʊ",
+  );
 }
-const encoder = new TextEncoder();
-words.sort((a_, b_) => {
-    const a = encoder.encode(a_.entry.form);
-    const b = encoder.encode(b_.entry.form);
-    const min = Math.min(a.length, b.length);
 
-    for (let i = 0; i < min; ++i) {
-        if (a[i] !== b[i]) {
-            return a[i] - b[i];
-        }
-    }
-    
-    if (a.length !== b.length) {
-        return a.length - b.length;
-    }
-    return 0;
-})
+words.sort((a, b) => a.entry.id - b.entry.id);
 
-Deno.writeTextFileSync('./output.json', JSON.stringify(Otmjson, null, 2));
+Deno.writeTextFileSync("./output.json", JSON.stringify(otmjson, null, 2));
